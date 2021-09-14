7fe1ac996a091d2b85a13aac8d517dab83bb7de5
juls2001 <20184434@student.curtin.edu.au>
2021-09-14 11:39:41 +0800

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
-
-# Press ⇧⌘⏎ to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium import webdriver
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+class WebScarper():
+
+     def __init__(self,driver_path,embassy_home,first_embassy,last_embassy):
+          self._driver = webdriver.Chrome(driver_path)
+          self._embassy_home = embassy_home
+
+          self.first_embassy = first_embassy # Web address of first alphabetical embassy
+          self.last_embassy = last_embassy # Web address of last alphabetical embassy
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+     def get_embassy_sites(self):
+          '''
+          Returns a list of the contact page of each Australian embassy
+          :return:
+          '''
+          self._driver.get(self._embassy_home)
+          aTagsInLi = self._driver.find_elements_by_css_selector('li a')
+          record = False
+          sites = []
+
+          for a in aTagsInLi:
+               if a.get_attribute('href') == self.first_embassy:
+                    record = True
+
+               if record == True:
+                    sites.append(a.get_attribute('href'))
+
+               if a.get_attribute('href') == self.last_embassy:
+                    record = False
+          return sites
+
+     def check_email(self):
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# driver = webdriver.Chrome("/opt/homebrew/bin/chromedriver")
+#
+# # driver.get('https://www.dfat.gov.au/about-us/our-locations/missions/our-embassies-and-consulates-overseas')
+#
+# # aTagsInLi = driver.find_elements_by_css_selector('li a')
+# #
+# # sites = []
+# start_embassy = "https://www.dfat.gov.au/about-us/our-locations/missions/australian-embassy-afghanistan"
+# # final_embassy = "https://www.dfat.gov.au/about-us/our-locations/missions/Pages/australian-embassy-zimbabwe"
+# # record = False
+#
+# driver.get("https://www.dfat.gov.au/about-us/our-locations/missions/australian-consulate-bosnia-and-herzegovina")
+#
+#
+#
+#
+#
+#
+# # for a in aTagsInLi:
+# #      if a.get_attribute('href') == start_embassy:
+# #           record = True
+# #
+# #      if record == True:
+# #           sites.append(a.get_attribute('href'))
+# #
+# #      if a.get_attribute('href') == final_embassy:
+# #           record = False
+# #
+# # for i in sites:
+# #      print(i)
+#
+# driver.close()
